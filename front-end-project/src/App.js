3c7b064ce87d24bda9a8a09740f3da5f558d641b
taminging <ktm30702@naver.com>
2017-10-25 13:05:17 +0900

    front test

---
+++ front-end-project/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          taminging
+      </div>
+    );
+  }
+}
+// react-create-app
+// add react@next
+// add react-dom@next
+// add prop-types@next
+export default App;
