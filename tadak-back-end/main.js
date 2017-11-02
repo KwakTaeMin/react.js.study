74752a160eba0cada3122fe049fcf51a5e7bd1a8
taminging <ktm30702@naver.com>
2017-11-02 13:38:00 +0900

    commit

--- tadak-back-end/main.js
+++ tadak-back-end/main.js
@@ -13,19 +13,80 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+
+var numUsers = 0;
+var userList = [];
+
 io.on('connection', function(socket){
     console.log('a user connected');
 
+    var addedUser = false;
+
+    socket.on('new message', function (data) {
+        io.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    socket.on('add user', function (username) {
+        if (addedUser) return;
+        userList.push(username);
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        console.log(userList);
+        socket.emit('login', {
+            numUsers: numUsers,
+            userList : userList
+        });
+
+        io.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
     socket.on('chat message', function(msg){
-      console.log('message: ' + msg);
-      io.emit('server message', msg);
+
+        io.emit('server message', msg);
     });
 
     socket.on('disconnect', function() {
-        console.log('user disconnect');
+
+        if (addedUser) {
+
+           --numUsers;
+           console.log("user indexOf : " + userList.indexOf(socket.username))
+           userList.slice(userList.indexOf(socket.username), 1);
+           console.log(userList);
+           socket.broadcast.emit('login', {
+               numUsers: numUsers,
+               userList : userList
+           });
+            // echo globally that this client has left
+           socket.broadcast.emit('user left', {
+               username: socket.username,
+               numUsers: numUsers
+           });
+        }
     });
 });
 
 http.listen(4000, function(){
-    console.log("listening on 4000 ");
+    console.log("listening on 4000");
 });
